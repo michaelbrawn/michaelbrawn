74093c36d705aa644766eabf3b9581844d7f0aaf
Michael Brawn <mbrawn@infotech.com>
2015-11-15 22:48:21 -0500

    Attempt at cap setup

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,42 +1,43 @@
-set :stage, :production
+# Automatic 'bundle install' after deploy
+require 'bundler/capistrano'
 
-# Simple Role Syntax
-# ==================
-# Supports bulk-adding hosts to roles, the primary
-# server in each group is considered to be the first
-# unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# Application name
+set :application, 'michaelbrawn'
 
-# Extended Server Syntax
-# ======================
-# This can be used to drop a more detailed server
-# definition into the server list. The second argument
-# something that quacks like a hash can be used to set
-# extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# BlueHost SSH user
+set :user, 'sticksn3_rails'
 
-# you can set custom ssh options
-# it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
-# you can see them in [net/ssh documentation](http://net-ssh.github.io/net-ssh/classes/Net/SSH.html#method-c-start)
-# set it globally
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
-# and/or per server
-# server 'example.com',
-#   user: 'user_name',
-#   roles: %w{web app},
-#   ssh_options: {
-#     user: 'user_name', # overrides user setting above
-#     keys: %w(/home/user_name/.ssh/id_rsa),
-#     forward_agent: false,
-#     auth_methods: %w(publickey password)
-#     # password: 'please use keys'
-#   }
-# setting per server overrides global ssh_options
+# App Domain
+set :domain, 'sticksnstonesart.ca'
 
-# fetch(:default_env).merge!(rails_env: :production)
+# We don't need sudo on BlueHost
+set :use_sudo, false
+
+# git is our SCM
+set :scm, :git
+
+# master is our default git branch
+set :branch, 'master'
+
+# Use local git repository
+set :repository, '.'
+
+# Checkout, compress and send a local copy
+set :deploy_via, :copy
+set :deploy_to, "/home/#{user}/rails_apps/#{application}"
+
+# We have all components of the app on the same server
+server domain, :app, :web, :db, :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+
+  # Touch tmp/restart.txt to tell Phusion Passenger about new version
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+  end
+end
+
+# Clean-up old releases
+after 'deploy:restart', 'deploy:cleanup'
